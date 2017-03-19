2170022e4289b350dbad875fb23959f245487b74
colin <sharon.colin@eemi.com>
2017-03-19 18:55:51 +0200

    connect with firebase

--- src/app.js
+++ src/app.js
@@ -7,6 +7,12 @@ class App extends Component {
 		return(
 			<div>
 				<h1> Hello WeChat </h1>
+				<form onSubmit={this.usernameSubmitHandler} className="username-container">
+					<div>
+					  <input type="text" onChange={this.usernameChangeHandler} placeholder="Enter a username..." required />
+					</div>
+				<input type="submit" value="Submit" />
+				</form>
 				<ChatRoom />
 			</div>
 		)
